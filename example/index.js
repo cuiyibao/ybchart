fd332f2751366b4c0e44343dfd8741978017beb6
cuiyibao <cuiyibao001@ke.com>
2019-05-08 17:41:50 +0800

    添加差距饼图完毕

--- example/index.js
+++ example/index.js
@@ -8,8 +8,36 @@ ybchart.init(App)
 
 ybchart.diffPie({
   shape: {
-    x: 100,
+    x: 110,
     y: 100,
-    r: 73
-  }
+    r: 73,
+    pW: 50,
+  },
+  color: ["#35aea2", "#2652cc", "#de4e96", "#e8a937"],
+  data: [
+    {
+      value: 10,
+      name: "25以下"
+    },
+    {
+      value: 50,
+      name: "25-35"
+    },
+    {
+      value: 20,
+      name: "35-50"
+    },
+    {
+      value: 20,
+      name: "50以上"
+    }
+  ],
+  navBar: {
+    textStyle: {
+      color: "#6775ad"
+    },
+    icon: "circle",
+    data: ["25以下", "25-35", "35-50", "50以上"]
+  },
+  width: 230
 })
--- src/core/diffPie/index.js
+++ src/core/diffPie/index.js
@@ -7,22 +7,79 @@ function DiffPie(opts) {
   var cX = opts.shape.x
   var cY = opts.shape.y
   var cR = opts.shape.r
+  var cW = opts.shape.pW
 
-  var arc1 = new zrender.Sector({
-    shape: {
-      cx: cX,
-      cy: cY,
-      r: cR,
-      r0: cR - 40,
-      startAngle: 0,
-      endAngle: 2 * Math.PI / 360 * 90,
-      clockwise: true
-    },
-    style: {
-      fill: "#2bc6c7"
-    }
+  var data = opts.data
+  var total = util.deepClone(data).reduce(function(obj1, obj2) {
+    var current = isNaN(obj1) ? obj1.value : obj1
+    return current + obj2.value
   })
-  group.add(arc1)
+  var sortData = util.deepClone(data).sort(function(obj1, obj2) {
+    return obj1["value"] - obj2["value"]
+  })
+  var _startAngle = 0;
+  var _endAngle = 0;
+  for (var i = 0; i < data.length; i ++) {
+    var _startAngle = i == 0 ? 0 : _endAngle
+    var _endAngle = _startAngle + 2 * Math.PI / 360 * (data[i]["value"] / total * 360)
+    var rdR = 0; // 每个图形的间距
+    util.deepClone(sortData).forEach(function(ele, ind) {
+      if (ele["name"] == data[i]["name"]) {
+        rdR = (data.length - ind + 1) * 4
+      }
+    })
+    var arc = new zrender.Sector({
+      shape: {
+        cx: cX,
+        cy: cY,
+        r: cR - rdR,
+        r0: cR - cW,
+        startAngle: _startAngle,
+        endAngle: _endAngle,
+        clockwise: true
+      },
+      style: {
+        fill: opts.color[i],
+        shadowColor: "#131858",
+        shadowBlur: 10,
+      }
+    })
+    group.add(arc)
+  }
+
+  // 导航
+  var index = 0
+  opts.navBar.data.forEach(function(ele, ind) {
+    var narY = 100
+    if ((80 + 70 * (ind - index)) > opts.width) {
+      narY = 125
+      index = ind
+    }
+    var circle = new zrender.Circle({
+      shape: {
+        cx: 30 + 70 * (ind - index),
+        cy: opts.shape.y + narY + 5,
+        r: 6
+      },
+      style: {
+        fill: opts.color[ind],
+      },
+    })
+  
+    group.add(circle)
+
+    var bar = new zrender.Text({
+      style: {
+        text: ele,
+        textFill: opts.navBar.textStyle.color,
+        textAlign: 'left',
+        textVerticalAlign: 'top'
+      },
+      position: [40 + 70 * (ind - index), opts.shape.y + narY]
+    })
+  
+    group.add(bar)
+  })
   
   return group
 }
